dc5a106d4241729624d20ed780a72c3405c32e48
AntonBakaev <anton4381@gmail.com>
2016-07-23 00:35:49 +0300

    xml-serialize attributes fixed

--- Core/Models/Execution.cs
+++ Core/Models/Execution.cs
@@ -6,50 +6,51 @@ using Common.Enums;
 
 namespace Core.Models
 {
-	[Serializable]
-	public class Execution
-	{
-		[XmlElement("executionId")]
-		public string ExecutionId { get; set; }
+    [Serializable]
+    [XmlType("execution")]
+    public class Execution
+    {
+        [XmlElement("executionId")]
+        public string ExecutionId { get; set; }
 
-		[XmlElement("cycleName")]
-		public string CycleName { get; set; }
+        [XmlElement("cycleName")]
+        public string CycleName { get; set; }
 
-		[XmlElement("issueKey")]
-		public string IssueKey { get; set; }
+        [XmlElement("issueKey")]
+        public string IssueKey { get; set; }
 
-		[XmlElement("testSummary")]
-		public string TestSummary { get; set; }
+        [XmlElement("testSummary")]
+        public string TestSummary { get; set; }
 
-		[XmlElement("project")]
-		public string Project { get; set; }
+        [XmlElement("project")]
+        public string Project { get; set; }
 
-		[XmlElement("versions")]
-		public string Versions { get; set; }
+        [XmlElement("versions")]
+        public string Versions { get; set; }
 
-		[XmlElement("components", typeof(ComponentType))]
-		public ComponentType Components { get; set; }
+        [XmlElement("components", typeof(ComponentType))]
+        public ComponentType Components { get; set; }
 
-		[XmlElement("priority", typeof(PriorityType))]
-		public PriorityType Priority { get; set; }
+        [XmlElement("priority", typeof(PriorityType))]
+        public PriorityType Priority { get; set; }
 
-		[XmlElement("executedBy")]
-		public string ExecutedBy { get; set; }
+        [XmlElement("executedBy")]
+        public string ExecutedBy { get; set; }
 
-		[XmlElement("executedOn")]
-		public string ExecutedOn { get; set; }
+        [XmlElement("executedOn")]
+        public string ExecutedOn { get; set; }
 
-		[XmlElement("executedStatus", typeof(ExecutedStatus))]
-		public ExecutedStatus ExecutedStatus { get; set; }
+        [XmlElement("executedStatus", typeof(ExecutedStatus))]
+        public ExecutedStatus ExecutedStatus { get; set; }
 
-		[XmlElement("executionDefects")]
-		public string ExecutionDefects { get; set; }
+        [XmlElement("executionDefects")]
+        public string ExecutionDefects { get; set; }
 
-		[XmlElement("creationDate")]
-		public string CreationDate { get; set; }
+        [XmlElement("creationDate")]
+        public string CreationDate { get; set; }
 
-		[XmlArray("teststeps")]
-		[XmlArrayItem("teststep", typeof(TestStep))]
-		public TestStep[] TestSteps { get; set; }
-	}
+        [XmlArray("teststeps")]
+        [XmlArrayItem("teststep", typeof(TestStep))]
+        public List<TestStep> TestSteps { get; set; }
+    }
 }
--- Core/Models/TestStep.cs
+++ Core/Models/TestStep.cs
@@ -4,28 +4,29 @@ using Common.Enums;
 
 namespace Core.Models
 {
-	[Serializable]
-	public class TestStep
-	{
-		[XmlElement("stepId")]
-		public string StepId { get; set; }
+    [Serializable]
+    [XmlType("teststep")]
+    public class TestStep
+    {
+        [XmlElement("stepId")]
+        public string StepId { get; set; }
 
-		[XmlElement("orderId")]
-		public int OrderId { get; set; }
+        [XmlElement("orderId")]
+        public int OrderId { get; set; }
 
-		[XmlElement("step")]
-		public string Step { get; set; }
+        [XmlElement("step")]
+        public string Step { get; set; }
 
-		[XmlElement("data")]
-		public string Data { get; set; }
+        [XmlElement("data")]
+        public string Data { get; set; }
 
-		[XmlElement("result")]
-		public string Result { get; set; }
+        [XmlElement("result")]
+        public string Result { get; set; }
 
-		[XmlElement("stepStatus", typeof(StepStatus))]
-		public StepStatus StepStatus { get; set; }
+        [XmlElement("stepStatus", typeof(StepStatus))]
+        public StepStatus StepStatus { get; set; }
 
-		[XmlElement("stepComment")]
-		public string StepComment { get; set; }
-	}
+        [XmlElement("stepComment")]
+        public string StepComment { get; set; }
+    }
 }
