24142ff95dfd608459528c01036d4ee327115ea8
Anton Zhmyrov <Anton.Zhmyrov@teaminternational.com>
2016-07-25 12:28:10 +0300

    added a vm builder

--- Core/VmBuilders/FlowStatisticsVmBuilder.cs
+++ Core/VmBuilders/FlowStatisticsVmBuilder.cs
@@ -1,13 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Enums;
+using Core.Models;
 using Core.ViewModels;
 
 namespace Core.VmBuilders
 {
 	public class FlowStatisticsVmBuilder
 	{
-		public FlowStatisticsVm BuildFlowStatisticsVm()
+		private const string RetailStr = "Shop_E2E: Retail";
+
+		public FlowStatisticsVm GetFlowStatisticsVm(List<Execution> executionsList)
 		{
-			throw new NotImplementedException();
+			return new FlowStatisticsVm
+			{
+				Passed = executionsList.Count(e => e.ExecutedStatus == ExecutedStatus.Pass),
+				Blocked = executionsList.Count(e => e.ExecutedStatus == ExecutedStatus.Blocked),
+				Failed = executionsList.Count(e => e.ExecutedStatus == ExecutedStatus.Fail),
+				Wip = executionsList.Count(e => e.ExecutedStatus == ExecutedStatus.WIP)
+			};
+		}
+
+		public FlowStatisticsVm GetRetailShopStatisticsVm(List<Execution> executionsList)
+		{
+			return GetFlowStatisticsVm(executionsList.Where(e => e.TestSummary.Trim().StartsWith(RetailStr)).ToList());
 		}
 	}
 }
