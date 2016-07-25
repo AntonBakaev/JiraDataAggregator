eb013d7ed6b7411c66f78cb0c47de1425e0d1433
teaakov <teaakov@telenor.dk>
2016-07-25 21:12:22 +0300

    ConvertHelper added

---
+++ Common/Helpers/ConvertHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.Helpers
+{
+	public class ConvertHelper
+	{
+		public static Dictionary<string, string> ToDictionary(object data)
+		{
+			if (data == null)
+				return null;
+
+			return data.GetType().GetProperties()
+				.ToDictionary(x => x.Name, x => x.GetGetMethod().Invoke(data, null) == null
+					? ""
+					: x.GetGetMethod().Invoke(data, null).ToString());
+		}
+	}
+}
