e421e8369c2d36b03bef055bc20dc7cf616106e8
Oleksii Kleims <Oleksii.Kleims@teaminternational.com>
2016-07-26 12:26:05 +0300

    calling rest client in repository

--- Core/Repositories/Interfaces/IDefectReportRepository.cs
+++ Core/Repositories/Interfaces/IDefectReportRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Enums;
 using Core.Models;
 
 namespace Core.Repositories.Interfaces
@@ -6,5 +8,7 @@ namespace Core.Repositories.Interfaces
 	public interface IDefectReportRepository
 	{
 		IEnumerable<Execution> GetIsitLaunchCriticalViewData(string filePath);
+
+		Task<IssueStatus> GetIssueStatus(string issueKey);
 	}
 }
