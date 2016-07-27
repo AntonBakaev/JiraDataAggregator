8cd5f6d3763c2d5404f3934dd979436f731718cc
Nataliia Martynenko <Nataliia.Martynenko@teaminternational.com>
2016-07-27 09:13:03 +0300

    +part generator

--- Common/Helpers/ReportGeneratorHelper.cs
+++ Common/Helpers/ReportGeneratorHelper.cs
@@ -4,52 +4,69 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Configuration;
 using System.IO;
+using System.Text;
+using Core.ViewModels;
 
 namespace Common.Helpers
 {
 	public class ReportGeneratorHelper
 	{
-		public static string GenerateRtfReport(string template, IDictionary<string, string> parameters = null)
-		{
-			var requestParameters = parameters != null
-				? parameters.ToDictionary(p => p.Key, p => p.Value)
-				: new Dictionary<string, string>();
-			foreach (Match match in Regex.Matches(template, @"\\\{(.*?)\\\}"))
-			{
-				var paramName = match.Groups[1].Value;
-				if (!requestParameters.ContainsKey(paramName))
-				{
-					throw new ArgumentException(string.Format("'{0}' must be defined to generate url", paramName), "parameters");
-				}
 
-				var paramValue = requestParameters[paramName];
-				if (paramValue == null)
-				{
-					throw new ArgumentException(string.Format("'{0}' must not be 'null'", paramName), "parameters");
-				}
+        public static string GenerateBlockingIssues(BlockingIssuesVm blockingIssues)
+        {
+            StringBuilder blockingIssuesPart = new StringBuilder();
+            foreach (var defect in blockingIssues.DefectsList)
+            {
+                string defectLine = string.Format("{0} - blocks {1} flows", defect.DefectName, defect.BlockingIssues.Count);
+                blockingIssuesPart.Append("\\line \\bullet ");
+                foreach (var issue in defect.BlockingIssues)
+                {
+                    string issueLine = string.Format("{0} {1}", issue.IssueName, issue.Link);
 
-				template = template.Replace(string.Format("\\{{{0}\\}}", paramName), paramValue);
-				requestParameters.Remove(paramName);
-			}
+                    blockingIssuesPart.Append("\\line \\tab \\u9632 " + issueLine + "}");
+                }
+            }
+            return blockingIssuesPart.ToString();
+        }
 
-			return template;
-		}
+        public static string GenerateRtfReport(string template, IDictionary<string, string> parameters)
+        {
+            foreach (Match match in Regex.Matches(template, @"\\\{(.*?)\\\}"))
+            {
+                var paramName = match.Groups[1].Value;
+                if (!parameters.ContainsKey(paramName))
+                {
+                    throw new ArgumentException(string.Format("'{0}' must be defined to generate url", paramName), "parameters");
+                }
+
+                var paramValue = parameters[paramName];
+                if (paramValue == null)
+                {
+                    throw new ArgumentException(string.Format("'{0}' must not be 'null'", paramName), "parameters");
+                }
+
+                template = template.Replace(string.Format("\\{{{0}\\}}", paramName), paramValue);
+                parameters.Remove(paramName);
+            }
+
+            return template;
+        }
 
 
-		public static void GenerateReport(Dictionary<string, string> allModels)
-		{
-			string path = ConfigurationManager.ConnectionStrings["TemplatePath"].ConnectionString;
-			string report_path = ConfigurationManager.ConnectionStrings["ReportPath"].ConnectionString;
+        public static void GenerateReport(Dictionary<string, string> allModels)
+        {
+            string path = ConfigurationManager.ConnectionStrings["TemplatePath"].ConnectionString;
+            string report_path = ConfigurationManager.ConnectionStrings["ReportPath"].ConnectionString;
 
-			string template = File.ReadAllText(path);
+            string template = File.ReadAllText(path);
 
-			string datetime = DateTime.Now.ToString("yyyy-M-d hh:mm");
+            string datetime = DateTime.Now.ToString("yyyy-M-d hh:mm");
 
-			allModels.Add("Date.Time", datetime);
+            allModels.Add("Date.Time", datetime);
 
-			template = GenerateRtfReport(template, allModels);
+            template = GenerateRtfReport(template, allModels);
 
-			File.WriteAllText(report_path, template);
-		}
+            File.WriteAllText(report_path, template);
+        }
 	}
 }
