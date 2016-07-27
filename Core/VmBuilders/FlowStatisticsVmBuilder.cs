2c8368d744f591b4661dbd5335959d401d97cfe4
Anton Bakaiev <Anton.Bakaiev@teaminternational.com>
2016-07-27 10:44:02 +0300

    FlowStatisticsVm modified and made serializable

--- Core/VmBuilders/FlowStatisticsVmBuilder.cs
+++ Core/VmBuilders/FlowStatisticsVmBuilder.cs
@@ -19,23 +19,12 @@ namespace Core.VmBuilders
 				Blocked = executionsList.Count(e => e.ExecutedStatus == ExecutedStatus.Blocked),
 				Failed = executionsList.Count(e => e.ExecutedStatus == ExecutedStatus.Fail),
 				Wip = executionsList.Count(e => e.ExecutedStatus == ExecutedStatus.WIP),
-				IsFiltered = false
 			};
 		}
 
 		public FlowStatisticsVm GetFlowStatisticsVmByFilter(IEnumerable<Execution> executionsList)
 		{
-			var newExecutionsList = executionsList.Where(e => e.TestSummary.Trim().StartsWith(RetailStr)).ToList();
-
-			return new FlowStatisticsVm
-			{
-				Passed = newExecutionsList.Count(e => e.ExecutedStatus == ExecutedStatus.Pass),
-				Blocked = newExecutionsList.Count(e => e.ExecutedStatus == ExecutedStatus.Blocked),
-				Failed = newExecutionsList.Count(e => e.ExecutedStatus == ExecutedStatus.Fail),
-				Wip = newExecutionsList.Count(e => e.ExecutedStatus == ExecutedStatus.WIP),
-				Filter = RetailStr,
-				IsFiltered = true
-			};
+			return GetFlowStatisticsVm(executionsList.Where(e => e.TestSummary.Trim().StartsWith(RetailStr)).ToList());
 		}
 	}
 }
