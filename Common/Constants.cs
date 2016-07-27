33a1aaed4b7ba054dbfbe0e42444cb50c29dddf6
Anton Zhmyrov <Anton.Zhmyrov@teaminternational.com>
2016-07-27 13:20:48 +0300

    added constants class

---
+++ Common/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+	public static class Constants
+	{
+		
+		public static class RestExceptions
+		{
+			public const string BadRequestError = "The request could not be understood by the server due to malformed syntax.";
+			public const string UnauthorizedError = "The request requires user authentication.";
+			public const string ForbiddenError = "The server understood the request, but is refusing to fulfill it.";
+			public const string NotFoundError = "The server has not found anything matching the Request-URI.";
+			public const string MethodNotAllowed = "The method specified in the Request-Line is not allowed for the resource identified by the Request-URI.";
+			public const string InternalServerError = "The server encountered an unexpected condition which prevented it from fulfilling the request.";
+			public const string DefaultError = "Unpredictable rest error occured.";
+		}
+	}
+}
