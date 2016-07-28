c6021505b73df1301fc1353c3578561a8ffb80d9
Anton Bakaiev <Anton.Bakaiev@teaminternational.com>
2016-07-28 12:27:23 +0300

    DefectKeyVm added

--- Core/VmBuilders/AllDefectKeysVmBuilder.cs
+++ Core/VmBuilders/AllDefectKeysVmBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Core.Models;
 using Core.ViewModels;
 using Core.VmBuilders.Interfaces;
@@ -10,11 +11,11 @@ namespace Core.VmBuilders
 	{
 		public AllDefectKeysVm GetAllBlockingDefects(IEnumerable<Execution> executionsList)
 		{
-			var defectsKeysList = new List<string>();
+			var defectsKeysList = new List<DefectKeyVm>();
 			foreach (var execution in executionsList)
 				foreach (var defect in execution.ExecutionDefects)
-					if (defect != String.Empty && !defectsKeysList.Contains(defect))
-						defectsKeysList.Add(defect);
+					if (defect != String.Empty && !defectsKeysList.Exists(d => d.Value == defect))
+						defectsKeysList.Add(new DefectKeyVm { Value = defect });
 
 			return new AllDefectKeysVm() { AllDefectKeys = defectsKeysList };
 		}
